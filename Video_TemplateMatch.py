eb90227c6fcaa986c1558dfa1333dcfa32783c0c
Barbara <if17b075@technikum-wien.at>
2018-11-18 19:39:56 +0100

    added array of found single-positions

--- Video_TemplateMatch.py
+++ Video_TemplateMatch.py
@@ -6,13 +6,16 @@ import colorsys
 VIDEO = './videos/29s.mp4'
 TEMPLATE = './images/legoBlue_50.JPG'
 RESCALE = 1 # set 1 to rescale the size of video and template
-SCALE = 50
+SCALE = 60
 THRESHOLD = 0.83
+DISTANCE = 10 # must be more than DISTANCE px between found positions
 
 
 # resize frame or template for efficiency/testing purposes
 def rescale(image, w, h, if_frame):
     percent = SCALE
+    #if if_frame == 0:
+    #    percent = int(percent - 4)
     w = int(w * percent/100)
     h = int(h * percent/100)
     return cv2.resize(image, (w, h), interpolation=cv2.INTER_AREA)
@@ -25,6 +28,15 @@ def get_colorHSV(image, w, h):
     color = cv2.mean(image[top_left[1]:top_left[1] + h, top_left[0]:top_left[0] + w])
     # print(color[2], color[1], color[0]) # RGB
     return colorsys.rgb_to_hsv(color[2], color[1], color[0])
+
+
+def find_single_position(array, element):
+    distance = DISTANCE
+    for i in range(len(array)):
+        if -distance < element[0] - array[i][0] < distance and -distance < element[1] - array[i][1] < distance:
+            break
+        if i == len(array) - 1:
+            array.append(element)
 
 
 # initialize the camera or video
@@ -72,6 +84,9 @@ while cap.isOpened():
         # if templateRotated used
         # loc = np.where((res >= threshold) | (res2 >= THRESHOLD))
 
+        legosBlue = []
+        legosRed = []
+
         for pt in zip(*loc[::-1]):  # list loc[<start>:<stop>:<step>]
             # get mean color (RGB) of found template
             color = cv2.mean(frame[pt[1]:pt[1] + h, pt[0]:pt[0]+w])
@@ -83,12 +98,29 @@ while cap.isOpened():
             # if blue TODO: to adapt
             if (0.55, 0.33, 141) <= colorHSV <= (0.65, 1, 255):
                 cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
+                blue = [pt[0] + w, pt[1] + h]
+
+                if legosBlue == []:
+                    legosBlue.append(blue)
+                else:
+                    find_single_position(legosBlue, blue)
 
             # if red TODO: to adapt
             if (0.92, 0.33, 200) <= colorHSV <= (1, 1, 255):
                 cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+                red = [pt[0] + w, pt[1] + h]
+
+                if legosRed == []:
+                    legosRed.append(red)
+                else:
+                    find_single_position(legosRed, red)
+
             # if yellow TODO: to found
             # not found with the template
+        print("Blue: ")
+        print(legosBlue)
+        print("Red: ")
+        print(legosRed)
 
         # show the frame
         cv2.imshow("Frame", frame)

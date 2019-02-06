cb5e22e188c6e7802ad2caa8c25e46c4001a4e67
Barbara <b.latosinska@gmail.com>
2019-02-06 22:53:19 +0100

    optimized lego recognition
    size, color, shape

--- RealSenseCamera_ReadVideo/RS_ShapeDetection.py
+++ RealSenseCamera_ReadVideo/RS_ShapeDetection.py
@@ -1,5 +1,4 @@
-# TODO: information about Apache License?
-# License: Apache 2.0. (pyrealsense2)
+# Used pyrealsense2 on License: Apache 2.0.
 
 # TODO: optimization possibilities:
 # temporal filtering (IIR filter) to remove "holes" (depth=0), hole-filling
@@ -11,8 +10,29 @@ import pyrealsense2 as rs
 import imutils
 import numpy as np
 import cv2
+import colorsys
+
+# For resolution 1280x720 and distance ~1 meter a short side of lego piece has ~14 px length
+WIDTH = int(1280)
+HEIGHT = int(720)
+# Side of lego piece
+MIN_LENGTH = 4
+MAX_LENGTH = 35
+# Objects in greater distance to the board than (1 +- CLIP) * x will be excluded from processing
+CLIP = 0.04
+# Aspect ratio for square and rectangle
+MIN_SQ = 0.7
+MAX_SQ = 1.3
+MIN_REC = 0.2
+MAX_REC = 2.5
+# accepted HSV colors
+BLUE_MIN = (0.53, 0.33, 141)
+BLUE_MAX = (0.65, 1, 255)
+RED_MIN = (0.92, 0.33, 170)
+RED_MAX = (1, 1, 255)
 
 
+# Check if the shape is the searched object
 def detect(contour):
     # Initialize the shape name and approximate the contour with Douglas-Peucker algorithm
     shape = "shape"
@@ -21,32 +41,69 @@ def detect(contour):
 
     # Check if the shape has 4 vertices
     if len(approx) == 4:
-        # Compute the bounding box of the contour and compute the aspect ratio
+        # Compute the rotated bounding box and draw all found objects (red)
+        # For testing purposes, later should be computed only in the loop and without drawing
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+
+        # Compute the bounding box of the contour and the aspect ratio
         (x, y, w, h) = cv2.boundingRect(approx)
-        ar = w / float(h)
 
         # Check the size and color of the shape to decide if it is the searched object
-        # TODO: find size of objects depend on distance to the board, check color
-        if (8 < h < 22) & (8 < w < 22):
-            # Check if it is a square or a rectangle
-            # TODO: maybe change the ratio
-            if 0.7 <= ar <= 1.3:
-                shape = "square"
-            elif 0.4 < ar < 2.2:
-                shape = "rectangle"
+        if (MIN_LENGTH < h < MAX_LENGTH) & (MIN_LENGTH < w < MAX_LENGTH):
+            shape = check_if_square(box)
     return shape
+
+
+# Check if square or rectangle
+def check_if_square(box):
+    sides_length = calculate_size(box)
+    # Compute the aspect ratio
+    ar = int(sides_length[0]) / int(sides_length[1])
+    shape = "shape"
+    print("_________")
+    if MIN_SQ <= ar <= MAX_SQ:
+        shape = "square"
+        print("Square size:", sides_length[0], sides_length[1])
+    elif MIN_REC < ar < MAX_REC:
+        shape = "rectangle"
+        print("Rectangle size:", sides_length[0], sides_length[1])
+    return shape
+
+
+# Calculate two sides from one of corners
+def calculate_size(box):
+    length = []
+    for idx in range(3):
+        length.append(np.linalg.norm(box[0] - box[idx+1]))
+    # Delete the highest value (diagonal), only two sides lengths are remaining in the array
+    return np.delete(length, np.argmax(length))
+
+
+# TODO: find correct RGB
+def check_color(x, y):
+    # calculate the mean color (RGB)
+    color = cv2.mean(color_image[y:y+4, x:x+4])
+    # print("RGB:", color[2], color[1], color[0])
+
+    colorHSV = colorsys.rgb_to_hsv(color[2], color[1], color[0])
+    print("HSV:", colorHSV)
+
+    if (RED_MIN <= colorHSV <= RED_MAX) | (BLUE_MIN <= colorHSV <= BLUE_MAX):
+        return True
+    return False
 
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-# TODO: use the max resolution (1280,720) and post processing with downsample 3
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, WIDTH, HEIGHT, rs.format.bgr8, 30)
 
-# TODO: find the middle of the frame to take distance to the board
-middleX = int(320)
-middleY = int(240)
+middleX = int(WIDTH/2)
+middleY = int(HEIGHT/2)
 # Initialize the clipping distance
 clip_dist = 0
 
@@ -70,6 +127,7 @@ try:
         # Get aligned frames
         aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
         color_frame = aligned_frames.get_color_frame()
+
         # Validate that both frames are valid
         if not aligned_depth_frame or not color_frame:
             continue
@@ -85,7 +143,7 @@ try:
 
         # Change background regarding clip_dist to black (depth image is 1 channel, color is 3 channels)
         depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
-        bg_removed = np.where((depth_image_3d > clip_dist * 1.1) | (depth_image_3d < clip_dist * 0.9), 0, color_image)
+        bg_removed = np.where((depth_image_3d > clip_dist * (1 + CLIP)) | (depth_image_3d < clip_dist * (1 - CLIP)), 0, color_image)
 
         # Render aligned images
         # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
@@ -133,10 +191,14 @@ try:
                 cX = int((M["m10"] / M["m00"]))
                 cY = int((M["m01"] / M["m00"]))
                 shape = detect(c)
-                if shape != 'shape':
-                    cv2.drawContours(frame, [c], -1, (0, 255, 0), 3)
-                    cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    print(cX, cY)
+                if shape != "shape":
+                    check = False
+                    # Check color (currently only red and blue accepted)
+                    check = check_color(cX, cY)
+                    print("Coordinates:", cX, cY, check)
+                    if check:
+                        cv2.drawContours(frame, [c], -1, (0, 255, 0), 3)
+                        cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         # Render shape detection images
         cv2.namedWindow('Shape detection', cv2.WINDOW_AUTOSIZE)

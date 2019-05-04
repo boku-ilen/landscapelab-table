aa58090d5fb27ce2a327a97b7c8e880fe3b99780
Barbara <b.latosinska@gmail.com>
2019-05-04 21:34:25 +0200

    Preparing requests for assest (create/delete)

--- RealSenseCamera_ReadVideo/Tracking/LegoBrick.py
+++ RealSenseCamera_ReadVideo/Tracking/LegoBrick.py
@@ -1,3 +1,24 @@
+from enum import Enum
+#from ..RS_ShapeDetection import ShapeDetector
+
+# Assets request URLs (lego bricks position)
+# Create an asset (.../id/pos_x/pos_y), return an instance_id
+REQUEST_CREATE_ASSET = "http://141.244.151.53/landscapelab/assetpos/create/"
+# Update an asset position (.../instance_id/pos_x/pos_y)
+REQUEST_SET_ASSET = "http://141.244.151.53/landscapelab/assetpos/set/"
+# TODO: is it "delete"?
+# Delete an asset (.../instance_id)
+REQUEST_DELETE_ASSET = "http://141.244.151.53/landscapelab/assetpos/delete/"
+
+
+# Define lego type ids
+class LegoTypeId(Enum):
+    SQUARE_RED = 1
+    SQUARE_BLUE = 1
+    RECTANGLE_RED = 2
+    RECTANGLE_BLUE = 2
+
+
 class LegoBrick:
     """Holder for lego brick properties"""
 
@@ -39,6 +60,19 @@ class LegoBrickCollections:
         # Create lego brick object
         lego_brick = LegoBrick(id, centroid, shape, color)
 
+        # HTTP request with a new object
+        # Match a type id based on contour name and color
+        lego_type_id = self.match_lego_type_id(shape, color)
+
+        # TODO: geo coordinates from shapeDetection.py!
+        # TODO: allow for relative import
+        # Calculate coordinates for detected lego bricks
+        # coordinates = self.calculate_coordinates(centroid)
+        # print("Detection recalculated: id:{}, coordinates:{}".format(coordinates))
+
+        print(REQUEST_CREATE_ASSET + str(lego_type_id) + "/" + str(centroid[0]) + "/" + str(centroid[1]))
+        # lego_instance_id = requests.get(REQUEST_CREATE_ASSET + str(lego_type_id) + "/" + str(centroid[0]) + "/" + str(centroid[1]))
+
         # Look for the related collection and save as dictionary
         if color == "red":
             if shape == "square":
@@ -46,7 +80,7 @@ class LegoBrickCollections:
             elif shape == "rectangle":
                 self.red_rcts_collection.append(lego_brick.__dict__)
                 for lego in self.red_rcts_collection:
-                    print(lego)
+                    print("LegoBrick", lego)
         elif color == "blue":
             if shape == "square":
                 self.blue_sqrs_collection.append(lego_brick.__dict__)
@@ -81,6 +115,9 @@ class LegoBrickCollections:
                 # If id is found, delete lego brick from related collection and stop searching
                 if collections_list[collection_idx][lego_idx]["id"] == id:
                     del collections_list[collection_idx][lego_idx]
+
+                    # TODO: delete altered instance (URL)
+
                     lego_brick_deleted = True
                     # TODO: remove LegoBrick reference?
                 lego_idx += 1
@@ -126,6 +163,25 @@ class LegoBrickCollections:
                     self.collections_empty = True
         else:
             self.collections_empty = False
+
+    # Match a type id of a lego brick
+    @staticmethod
+    def match_lego_type_id(contour_name, shape):
+
+        lego_type_id = 0
+
+        # Match the type using the defined Enum
+        if contour_name is "square" and shape is "red":
+            lego_type_id = LegoTypeId.SQUARE_RED.value
+        elif contour_name is "square" and shape is "blue":
+            lego_type_id = LegoTypeId.SQUARE_BLUE.value
+        elif contour_name is "rectangle" and shape is "red":
+            lego_type_id = LegoTypeId.RECTANGLE_RED.value
+        elif contour_name is "rectangle" and shape is "blue":
+            lego_type_id = LegoTypeId.RECTANGLE_BLUE.value
+
+        # Return the lego type id
+        return lego_type_id
 
 
 if __name__ == "__main__":

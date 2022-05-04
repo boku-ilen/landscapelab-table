6333456e2be14a4305ab083da3dc9f485b574242
Karl <karl.bittner@hexaquo.at>
2022-05-04 13:15:01 +0200

    Proposed changes to communication protocol

--- LabTable/Communication/LLCommunicator.py
+++ LabTable/Communication/LLCommunicator.py
@@ -11,46 +11,51 @@ from LabTable.ExtentTracker import ExtentTracker
 logger = logging.getLogger(__name__)
 
 # remote communication protocol
-GET_ASSETS_MSG = {
-    "keyword": "GET_OBJECT_LAYER_DATA",  # FIXME: do we need this? could be provided during ProgramStage update?
-}
-CREATE_OBJECT_MSG = {
+SEND_REC_CREATE_OBJECT_MSG = {
     "keyword": "CREATE_OBJECT",
     "position": [0.0, 0.0],
     "brick_color": None,
     "brick_shape": None
 }
-UPDATE_OBJECT_MSG = {
-    "keyword": "SET_OBJECT_POSITION",  # FIXME: could other parameters be changed by the LabTable?
+SEND_REC_UPDATE_OBJECT_MSG = {
+    "keyword": "SET_OBJECT_POSITION",
     "object_id": 0,
     "position": [0.0, 0.0]
 }
-REMOVE_OBJECT_MSG = {
+SEND_REC_REMOVE_OBJECT_MSG = {
     "keyword": "REMOVE_OBJECT",
     "object_id": 0
 }
-OBJECT_ANSWER_MSG = {  # FIXME: do we need more parameters? or do we transfer the serialized Brick-Object?
+REC_OBJECT_ANSWER_MSG = {  # Answer to create, update, remove
     "keyword": "OBJECT_ANSWER",
     "success": False,
     "object_id": 0
 }
-HANDSHAKE_MSG = {
+SEND_HANDSHAKE_MSG = {
     "keyword": "HANDSHAKE",
-    "detected_brick_shapes": [],  # FIXME: or is it necessary to provide the combination of shape and color?
+    "detected_brick_shape_color_pairs": [],  # [[shape, color], ...]
     "detected_brick_colors": []
 }
-UPDATE_GAMESTAGE_MSG = {
-    "keyword": "CHANGE_GAMESTAGE",
-    "used_brick_types": [],
+REC_GAMESTATE_INFO_MSG = {  # Received after the first handshake and if the gamestate changes
+    "keyword": "GAMESTATE_INFO",
+    "used_bricks": [],  # [[shape, color, icon_svg, disappear_after_seconds], ...]
+    "scores": [],  # [[id, max_value], ...]
+    "start_position": [0.0, 0.0],
+    "start_extent": [0.0, 0.0],  # FIXME: or zoom level?
+    "projection": ""  # EPSG Code (optional, default is Austria Lambert)
 }
-SET_EXTENT_MSG = {  # this is sent on change to the LL
+REC_UPDATE_SCORE_MSG = {
+    "score_id": 0,
+    "value": 0
+}
+SEND_SET_EXTENT_MSG = {  # this is sent on change to the LL
     "keyword": "TABLE_EXTENT",
     "min_x": 0.0,
     "min_y": 0.0,
     "max_x": 0.0,
     "max_y": 0.0
 }
-PLAYER_POSITION_MSG = {  # this is received on change from LL
+REC_PLAYER_POSITION_MSG = {  # this is received on change from LL
     "keyword": "PLAYER_POS",
     "projected_x": 0.0,
     "projected_y": 0.0
@@ -83,7 +88,7 @@ class LLCommunicator(Communicator):
                 group, entry = key.split("-")
                 self.config.set(group, entry, response[key])
 
-        message = HANDSHAKE_MSG
+        message = SEND_HANDSHAKE_MSG
         # TODO: maybe fetch information about own system and send it to the LL
         self.send_message(message, settings_callback)
 
@@ -116,7 +121,7 @@ class LLCommunicator(Communicator):
         Extent.calc_world_pos(brick, self.extent_tracker.board, self.extent_tracker.map_extent)
 
         # Send request creating remote brick instance and save the response
-        message = CREATE_OBJECT_MSG.copy()
+        message = SEND_REC_CREATE_OBJECT_MSG.copy()
         # TODO: send the brick type
         message["position"] = [brick.map_pos_x, brick.map_pos_y]
 
@@ -136,13 +141,15 @@ class LLCommunicator(Communicator):
                 else:
                     logger.warning("could not remove remote brick {}".format(brick_instance))
 
-        message = REMOVE_OBJECT_MSG.copy()
+        message = SEND_REC_REMOVE_OBJECT_MSG.copy()
         message["object_id"] = brick_instance.object_id
 
         # Send a request to remove brick instance
         self.send_message(message, remove_callback)
 
     def get_stored_brick_instances(self, result_callback):
+
+        # FIXME: Can probably be removed
 
         def get_instances_callback(response: dict):
             stored_assets = response["objects"]
@@ -197,7 +204,7 @@ class LLCommunicator(Communicator):
             # TODO: we might want to validate if the target successfully accepted the extent change
             pass
 
-        message = SET_EXTENT_MSG.copy()
+        message = SEND_SET_EXTENT_MSG.copy()
         message["min_x"] = extent.x_min
         message["min_y"] = extent.y_min
         message["max_x"] = extent.x_max
